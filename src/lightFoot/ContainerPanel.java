0d89bd861e67896a2730949cf6cbc2b730bbfdf0
Sta1rZ <alan.longuet@supelec.fr>
2015-07-05 01:38:54 +0200

    ContainerPanel code

--- src/lightFoot/ContainerPanel.java
+++ src/lightFoot/ContainerPanel.java
@@ -7,23 +7,45 @@ import java.awt.event.MouseMotionListener;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
+import utils.Index2D;
 import utils.LayeredPane;
 
 public class ContainerPanel extends LayeredPane {
 	
 	private DraggablePanel draggablePanel;
 	private TransparentPanel transparentPanel;
+	private EventHandler eventHandler;
+	private int width;
+	private int height;
 	
 	
 	public ContainerPanel(int width,int height)
 	{
+		this.width=width;
+		this.height=height;
 		draggablePanel = new DraggablePanel(this,width,height);
 		transparentPanel = new TransparentPanel(this);
+		eventHandler = new EventHandler();
 		add(draggablePanel,JLayeredPane.DEFAULT_LAYER);
 		add(transparentPanel,JLayeredPane.DRAG_LAYER);
+		transparentPanel.addMouseListener(eventHandler);
+		transparentPanel.addMouseMotionListener(eventHandler);
 	}
-	
-	
+	public boolean addPanel(JPanel panel,int x,int y)
+	{
+		if(x >= width ||y >= height)
+		{
+			return false;
+		}
+		else
+		{
+			return draggablePanel.addPanel(panel,new Index2D(x,y));
+		}
+	}
+	public boolean addPanel(JPanel panel)
+	{
+		return draggablePanel.addPanel(panel);
+	}
 	
 	
 	
@@ -41,8 +63,8 @@ public class ContainerPanel extends LayeredPane {
 	class EventHandler implements MouseListener,MouseMotionListener
 	{
 
-		public void mouseDragged(MouseEvent arg0) {
-			// TODO Auto-generated method stub
+		public void mouseDragged(MouseEvent e) {
+			transparentPanel.fireDragEvent(new Pixel(e.getX(),e.getY()));
 			
 		}
 
@@ -71,8 +93,8 @@ public class ContainerPanel extends LayeredPane {
 			
 		}
 
-		public void mouseReleased(MouseEvent arg0) {
-			// TODO Auto-generated method stub
+		public void mouseReleased(MouseEvent e) {
+			transparentPanel.fireMouseReleaseEvent(new Pixel(e.getX(),e.getY()));
 			
 		}
 		
