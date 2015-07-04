e0ffbd7820f4b9291aa4a1d5f4b674e2119f8ea8
hermas55 <mhermas@uvw.fr>
2015-07-04 23:43:10 +0200

    Structure TransparentPanel

--- src/lightFoot/TransparentPanel.java
+++ src/lightFoot/TransparentPanel.java
@@ -1,13 +1,19 @@
 import javax.swing.JPanel;
 
 
+package lightFoot;
+
 public class TransparentPanel extends JPanel {
 	
 	TransparentPanel (ContainerPanel container){
 		
 	}
 
-	private  void receive(JPanel panel, Pixel pixel){
+	private void receive(JPanel panel, Pixel pixel){
+		
+	}
+	
+	private void fireDragEvent(){
 		
 	}
 }
