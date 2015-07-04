15b1436aae3a4c44990cfa9584310701e74f7aa1
KThrace <th.begeot@gmail.com>
2015-07-04 23:45:35 +0200

    dd

--- src/lightFoot/DraggablePanel.java
+++ src/lightFoot/DraggablePanel.java
@@ -1,5 +1,26 @@
 package lightFoot;
+import java.awt.GridLayout;
+import java.util.ArrayList;
 
-public class DraggablePanel {
+import javax.swing.JPanel;
 
+public class DraggablePanel extends JPanel {
+	
+	
+	public DraggablePanel(int column, int line){
+		
+		setLayout(new GridLayout(column, line));
+		
+		
+	}
+	
+	public void addPanel(JPanel panel){
+		
+	}
+	
+	
+	
+	
+	
+	
 }
