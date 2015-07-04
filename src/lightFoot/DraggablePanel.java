f400797a5d6bb65c46509e21d4b63fe38bdef464
KThrace <th.begeot@gmail.com>
2015-07-04 23:54:47 +0200

    dd2

--- src/lightFoot/DraggablePanel.java
+++ src/lightFoot/DraggablePanel.java
@@ -1,21 +1,31 @@
 package lightFoot;
 import java.awt.GridLayout;
-import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.JPanel;
 
+
 public class DraggablePanel extends JPanel {
 	
+	private HashMap<Pixel, Tile> hashTile;
+	private int nMaxTile;
+	private int nTile;
 	
-	public DraggablePanel(int column, int line){
+	public DraggablePanel(int width, int height){
 		
-		setLayout(new GridLayout(column, line));
-		
-		
+		setLayout(new GridLayout(width, height));
+		hashTile = new HashMap<Pixel, Tile>();
+		nMaxTile = width*height;
+		nTile = 0;
 	}
 	
+	public boolean isFull(){
+		return !(nTile < nMaxTile);
+	}
+	
+	
 	public void addPanel(JPanel panel){
-		
+		nTile++;
 	}
 	
 	
