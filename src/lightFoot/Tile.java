264bb84df5e8c5e2b8acd76b3f1c53b1c7f1d559
Mehdi005 <mohamedelmehdi.ayat@supelec.fr>
2015-07-05 01:51:05 +0200

    javadoc

--- src/lightFoot/Tile.java
+++ src/lightFoot/Tile.java
@@ -7,19 +7,35 @@ import javax.swing.JPanel;
 import utils.Index2D;
 
 public class Tile extends JPanel {
-	
+
 	private JPanel content;
 	private Index2D index;
-	
+
+	/**
+	 * Constructeur
+	 * @param content : panel contenu de la tile
+	 * @param index	: index de la tile dans la grille
+	 */
 	public Tile(JPanel content, Index2D index) {
 		this.content = content;
 		this.index = index;
 	}
 	
-	public Index2D getIndex() {
-		return index;
+	/**
+	 * Constructeur
+	 * @param index : index de la tile dans la grille
+	 */
+	public Tile(Index2D index) {
+		this.index = index;
+		this.content = null;
 	}
 
+
+	/**
+	 * Méthode qui ajoute le panel comme contenu de la tile si elle n'est pas vide et ne fais rien sinn
+	 * @param panel
+	 * @return vrai si l'ajout a été effectué avec succès
+	 */
 	public boolean addPanel(JPanel panel) {
 		if(!isEmpty()) return false;
 		else {
@@ -27,13 +43,23 @@ public class Tile extends JPanel {
 			this.add(content);
 			return true;
 		}
-		
+
 	}
-	public boolean contain(Pixel pixel) {
+
+	/**
+	 * Méthode qui rend vrai si le pixel est contenu dans la tile
+	 * @param pixel
+	 * @return vrai si le pixel est contenu dans la tile
+	 */
+	public boolean contains(Pixel pixel) {
 		Rectangle bounds = this.getBounds();
 		return bounds.contains(pixel.getX(), pixel.getY());
 	}
-	
+
+	/**
+	 * 
+	 * @return return vrai si la tile n'est pas vide faux sinn
+	 */
 	public boolean isEmpty() {
 		return content == null;
 	}
@@ -41,9 +67,13 @@ public class Tile extends JPanel {
 	public void setContent(JPanel content) {
 		this.content = content;
 	}
-	
+
 	public JPanel getContent() {
 		return content;
+	}
+
+	public Index2D getIndex() {
+		return index;
 	}
 
 }
