37a499b04a6a538758e014dafb3c7f587b6eb60f
hermas55 <mhermas@uvw.fr>
2015-07-05 00:44:34 +0200

    hermas

--- src/lightFoot/TransparentPanel.java
+++ src/lightFoot/TransparentPanel.java
@@ -3,20 +3,25 @@ package lightFoot;
 import javax.swing.JPanel;
 
 
-import javax.swing.JPanel;
-
-
 public class TransparentPanel extends JPanel {
+	
+	private ContainerPanel container;
 	
 	TransparentPanel (ContainerPanel container){
 		
 	}
 
 	public void receive(JPanel panel, Pixel pixel){
-		
+		/*if(container.getTile(pixel).isEmpty()) container.getTile(pixel).setContent(panel);
+			else ...
+		*/
 	}
 	
 	public void fireDragEvent(){
+		
+	}
+	
+	public void fireMouseReleaseEvent(){
 		
 	}
 }
