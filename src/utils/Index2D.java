985bae271917d588490ceaa387cfa14f8b9e626f
KThrace <th.begeot@gmail.com>
2015-07-05 01:20:56 +0200

    ii

---
+++ src/utils/Index2D.java
@@ -0,0 +1,24 @@
+package utils;
+
+public class Index2D {
+
+	private int i , j;
+	
+	public Index2D(int i, int j){
+		this.i = i;
+		this.j = j;
+	}
+	
+	public int getI(){
+		return this.i;
+	}
+	public int getJ(){
+		return this.j;
+	}
+	public void setIJ(int i, int j){
+		this.i = i;
+		this.j = j;
+	}
+	
+	
+}
