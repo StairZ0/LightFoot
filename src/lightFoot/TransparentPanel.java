d3326e21866bfbf4d30382acc75e98cabf28e17c
hermas55 <mhermas@uvw.fr>
2015-07-05 01:16:48 +0200

    zds

--- src/lightFoot/TransparentPanel.java
+++ src/lightFoot/TransparentPanel.java
@@ -1,5 +1,7 @@
 package lightFoot;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 
@@ -7,21 +9,32 @@ public class TransparentPanel extends JPanel {
 	
 	private ContainerPanel container;
 	
+	private JPanel current;
+	
 	TransparentPanel (ContainerPanel container){
-		
+		container = this.container;
+		setOpaque(false);
 	}
 
 	public void receive(JPanel panel, Pixel pixel){
-		/*if(container.getTile(pixel).isEmpty()) container.getTile(pixel).setContent(panel);
-			else ...
-		*/
-	}
-	
-	public void fireDragEvent(){
+		Dimension size = panel.getSize();
+		current = panel;
+		add(current);
+		current.setLocation(pixel.getX(), pixel.getY());
+		current.setSize(size);
 		
 	}
 	
-	public void fireMouseReleaseEvent(){
-		
+	public void fireDragEvent(Pixel pixel){
+		current.setLocation(pixel.getX(), pixel.getY());
+		repaint();
+	}
+	
+	public void fireMouseReleaseEvent(Pixel pixel){
+		remove(current);
+		container.sendToDraggablePanel(current, pixel);
+		revalidate();
+		// container.remove(pixel); container.add(current); conbtainer.repaint();
+		repaint();
 	}
 }
