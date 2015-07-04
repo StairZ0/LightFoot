88ca73527b9f47ff7a749a30580c2acc1c61a919
hermas55 <mhermas@uvw.fr>
2015-07-05 00:26:26 +0200

    zazzz

--- src/lightFoot/TransparentPanel.java
+++ src/lightFoot/TransparentPanel.java
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
 package lightFoot;
 
 import javax.swing.JPanel;
 
 
-=======
-package lightFoot; 
 import javax.swing.JPanel;
 
->>>>>>> origin/master
+
 public class TransparentPanel extends JPanel {
 	
 	TransparentPanel (ContainerPanel container){
