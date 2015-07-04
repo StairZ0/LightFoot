91835fae4b3e814c433e28536d2017bc102e4b3a
Sta1rZ <alan.longuet@supelec.fr>
2015-07-04 23:30:26 +0200

    Add ContainerPanel Structure

--- src/lightFoot/ContainerPanel.java
+++ src/lightFoot/ContainerPanel.java
@@ -1,5 +1,88 @@
 package lightFoot;
 
-public class ContainerPanel {
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
-}
+import javax.swing.JPanel;
+
+public class ContainerPanel extends JPanel {
+	
+	private DraggablePanel draggablePanel;
+	private TransparentPanel transparentPanel;
+	
+	
+	public ContainerPanel(int width,int height)
+	{
+		
+	}
+	private void sendToTransparentPanel(JPanel panel,Pixel pixel )
+	{
+		
+	}
+	private void sendToDraggablePanel(JPanel panel,Pixel pixel)
+	{
+		
+	}
+	
+	
+	
+	
+	
+	
+	class EventHandler implements MouseListener,MouseMotionListener
+	{
+
+		public void mouseDragged(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mouseMoved(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mouseClicked(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+} 
+
